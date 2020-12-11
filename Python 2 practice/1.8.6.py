1f3bf5a1f3c0fd4e522a1d6a8a4e7dc8ba655c52
cuilei0710 <cuilei0710@icloud.com>
2020-12-11 18:01:47 +0800

    leilei

---
+++ Python 2 practice/1.8.6.py
@@ -0,0 +1,4 @@
+#Author: 崔小葵
+#Date: 2020-12-10
+bacon = 20
+print(bacon + 1)
---
+++ Python 2 practice/1.8.7.py
@@ -0,0 +1,6 @@
+#Author: 崔小葵
+#Date: 2020-12-10
+message = 'spam' + 'spamspam'
+message_1 = 'spam' * 3
+print(message)
+print(message_1)
